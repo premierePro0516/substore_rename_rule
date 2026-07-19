40a16c634679fc5e121d1ad6e45ebe1ea998a4b4
PremierePro <116933669+premierePro0516@users.noreply.github.com>
2026-07-20 07:20:38 +0800

    Update rename_self.js

--- rename_self.js
+++ rename_self.js
@@ -71,7 +71,8 @@ const specialRegex = [
 
 // 内置信息节点清理正则（clear=true 时生效）
 const nameclear =
-  /(套餐|到期|有效|剩余|版本|已用|过期|失联|测试|官方|网址|备用|群|TEST|客服|网站|获取|订阅|流量|机场|下次|官址|联系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL)/i;
+  /(版本|已用|过期|失联|测试|官方|网址|备用|群|TEST|客服|网站|获取|订阅|流量|机场|下次|官址|联系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL)/i;
+//   /(套餐|到期|有效|剩余|版本|已用|过期|失联|测试|官方|网址|备用|群|TEST|客服|网站|获取|订阅|流量|机场|下次|官址|联系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL)/i;
 
 // IEPL/IPLC 同时识别分隔写法
 // prettier-ignore
